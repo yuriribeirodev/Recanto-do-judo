815bd58dad17103d3ccec6997c4588918cc804db
mirnapenaforte <mirnapenaforte.dev@gmail.com>
2025-11-04 12:01:23 -0300

    alterações

---
+++ src/pages/Section-ConhecaoProjeto.tsx
@@ -0,0 +1,23 @@
+export function ConhecaOPorjeto(){
+    return(
+        <>
+            <div className="containerConhecaOProjeto">
+
+
+            <h2>Conheça o Projeto</h2>
+            
+            <p>O Recanto Judô é um projeto social sem fins lucrativos criado para levar o judô ao bairro COHAB e promover inclusão por meio do esporte. Fundado por Gildean dos Santos após conquistar a faixa preta, começou em uma igreja e hoje funciona na maçonaria, com apoio da comunidade e do vereador Raimundo Penha. Hoje tem seis atletas classificados para competições nacionais.O projeto reforça valores como disciplina, esforço e cidadania, e oferece novas oportunidades para jovens e adultos da comunidade.</p>
+            
+            <button>Apoie</button>
+            
+            <div className="img">
+                <img src="public/imgSobrenos.png" alt="Imagem de Alunos do projeto Recanto do Judô" />
+            </div>
+        
+            </div>
+        
+        </>
+    )
+
+
+}
