df1cdb5f4e6a579c6870c0195a0fe818bf202d59
Yuri Ribeiro <yuriribeirodev@gmail.com>
2025-11-05 10:54:27 -0300

    feat: icones

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,14 @@
+import { Icon } from "@iconify/react";
+
 export function Card() {
     return(
-        <div className="bg-azul-500">
-            <div>
-                <h2 className="">Hello</h2>
+    <div className="bg-branco-fundoCards w-80% h-80% rounded-md ">
+        <div className="flex">
+            <div className="border-50% bg-azul-fundoIcones">
+                <Icon icon="mingcute:lotus-line" width="24" height="24"/>
             </div>
+            <h2 className="font-poppins text-vermelho-botao text-2xl">Endereço</h2>
         </div>
+    </div>    
     )
 }
--- src/pages/CardSection.tsx
+++ src/pages/CardSection.tsx
@@ -3,6 +3,9 @@ import { Card } from "../components/card"
 
 export function CardSection(){
     return(
+    <section className="bg-azul-fundoSection flex justify-center gap-3 h-50">
         <Card/>
+        <Card/>
+    </section>   
     )
 }
