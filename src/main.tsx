768b97658592008f9700c8fb831df71f685d4da9
mirnapenaforte <mirnapenaforte.dev@gmail.com>
2025-11-05 22:31:35 -0300

    feat: Criação da section depoimento do criador e adição de responsividade.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CardSection } from './pages/CardSection.tsx'
-import { Footer } from './pages/footer.tsx'
+import { Footer } from './pages/Footer.tsx'
 import { ConhecaOProjeto } from './pages/Section-ConhecaoProjeto.tsx'
+import { DepoimentoCriador } from './pages/Section-DepoimentoCriador.tsx'
 import './styles/index.css'
 import Header from './pages/Header.tsx'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header/>
     <ConhecaOProjeto/>
+    <DepoimentoCriador/>
     <CardSection/>
     <Footer/>
   </StrictMode>,
