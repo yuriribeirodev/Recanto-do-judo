eaeb021b9e4485d4534d24144588048796ed8f96
Yuri Ribeiro <yuriribeirodev@gmail.com>
2025-11-12 17:37:54 -0300

    feat: galeria com fotos

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -8,20 +8,23 @@ import {
 
 export function Gallery() {
     return(
-        <section className="flex flex-col w-full p-16 justify-center">
+        <section className="flex flex-col items-center w-full p-8 md:p-16 mx:auto">
+            <div className="flex flex-col gap-16 w-4/5">
              <h2 className="text-vermelho-botao text-4xl font-bold font-inter">Galeria</h2>
                 <Carousel>
                 <CarouselContent>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="fundo.png"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-kid1.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-kid2.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-kid3.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-kid4.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-kid5.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-turma.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3"><img src="foto-golpe.JPG" className="aspect-square object-cover rounded-xl w-full"/></CarouselItem>
                 </CarouselContent>
                 <CarouselPrevious/>
                 <CarouselNext/>
                 </Carousel>
+                </div>
         </section>
         
     )
